cbeef7f935c4cb32adf159c33bd01ae7de4269d6
tom <tom@tomgao.cc>
2023-11-16 22:47:57 +0800

    chore: update deps and change unbuild to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  target: ['es2021', 'node16'],
+  clean: true,
+  dts: true,
+  sourcemap: false,
+  splitting: true,
+});
