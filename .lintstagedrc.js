61ae25e1b9d384ec07688453b9f7568fd67e9e8a
Tom Gao <tom@tomgao.cc>
2023-12-27 21:14:52 +0800

    chore: lint-staged add stylelint config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,6 @@
 module.exports = {
-  '*.{js,ts,vue}': ['eslint --fix', 'prettier --write'],
+  '*.{js,cjs,ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.{json,md}': ['prettier --write'],
+  '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write'],
+  '*.{css,scss,less,html}': ['stylelint --fix', 'prettier --write'],
 };
